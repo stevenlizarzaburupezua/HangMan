56ea81aa4687f6e3c2186fb7172807e59f608711
Steven Lizarzaburu Pezúa <lizarzaburupezuasteven@gmail.com>
2022-11-25 21:45:55 -0500

    Generando las palabras aleatorias

--- HangManApp/MainPage.xaml.cs
+++ HangManApp/MainPage.xaml.cs
@@ -1,12 +1,27 @@
 ﻿using System.ComponentModel;
+using System.Diagnostics;
 
 namespace HangManApp;
 
 public partial class MainPage : ContentPage, INotifyPropertyChanged
 {
-	#region Fields
+	#region UI Properties
 
-	List<string> words = new List<string>()
+	public string SpotLight 
+	{ 
+		get => spotLight;
+		set
+		{
+            spotLight = value;
+			OnPropertyChanged();
+		} 
+	}
+
+    #endregion
+
+    #region Fields
+
+    List<string> words = new List<string>()
 	{
 		"python",
 		"javascript",
@@ -22,13 +37,18 @@ public partial class MainPage : ContentPage, INotifyPropertyChanged
 		"hotreload",
 		"snipetts"
 	};
-	string answer = string.Empty;
-	#endregion
+    string answer = "";
+	private string spotLight;
+	List<char> guessed = new List<char>();
 
-	public MainPage()
+    #endregion
+
+    public MainPage()
 	{
 		InitializeComponent();
+		BindingContext = this;
 		PickWord();
+		CalculateWord(answer, guessed);
 	}
 
 	#region Game Engine
@@ -36,6 +56,14 @@ public partial class MainPage : ContentPage, INotifyPropertyChanged
 	private	void PickWord()
 	{
 		answer = words[new Random().Next(0,words.Count)];
+	}
+
+	private void CalculateWord(string answer, List<char> guessed)
+	{
+		var temp = 
+			answer.Select(x => (guessed.IndexOf(x) >= 0 ? x : '_' ))
+			.ToArray();
+		SpotLight = string.Join(' ', temp);
 	}
 
 	#endregion
